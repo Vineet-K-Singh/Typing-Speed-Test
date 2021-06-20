9c156e79a423e90fa314b9e26edbb728660fcdb4
Vineet-K-Singh <vineetksingh1920@gmail.com>
2021-06-21 01:20:42 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import './components/textField.css';
+import TextInput from './components/TextInput';
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Typing Speed</h1>
+      <TextInput/>
+    </div>
+  );
+}
+
+export default App;
